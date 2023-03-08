7c78707141cbb01416dee1943b714ff516d3e43d
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2023-03-08 07:52:45 +0000

    Update my_scriptlets.js

--- Others/my_scriptlets.js
+++ Others/my_scriptlets.js
@@ -79,25 +79,21 @@
 /// click-element.js
 /// alias ce.js
 // [required] 1: element to click; 
-// [optional] 2: url should match given token; 3: should not click when certain cookie is already set; 4: timeout, if not set, it will click directly.
+// [optional] 2: url should match given token; 3: timeout, if not set, it will click directly.
 // example.com##+js(ce, element)
-// example.com##+js(ce, element, href, cookie, timeout)
-// example.com##+js(ce, element, , , timeout)
+// example.com##+js(ce, element, href, timeout)
+// example.com##+js(ce, element, , timeout)
 (() => {
     let selector = '{{1}}';
     if ( selector === '' || selector === '{{1}}' ) {
         return;
     }
-    // let href = '{{2}}';
-    // if ( href === ''  || href === '{{2}}' ) {
-    //     href = '';
-    // }
-    // let cookie = '{{3}}';
-    // if ( cookie === '' ) {
-    //     cookie = -1;
-    // }
-    let msecs = '{{2}}';
-    if ( msecs === '{{2}}' ) {
+    let href = '{{2}}';
+    if ( href === ''  || href === '{{2}}' ) {
+        href = '';
+    }
+    let msecs = '{{3}}';
+    if ( msecs === '{{3}}' ) {
         msecs = '';
     }
     let timeout = parseInt(msecs, 10);
@@ -110,7 +106,7 @@
             element.click();
         }
     };
-    // if ( (window.location.href.indexOf(href) !== -1) && (document.cookie.indexOf(cookie) == -1) ) {
+    if (window.location.href.indexOf(href) !== -1 ) {
         setTimeout(function() {
             if ( document.readyState === 'interactive' ||
                  document.readyState === 'complete' ) {
@@ -119,39 +115,35 @@
                 addEventListener('DOMContentLoaded', click);
             }
         }, timeout);
-    // }
+    }
 })();
 
 // Taken from AdGuard
 /// click-element-observer.js
 /// alias ceo.js
 // [required] 1: element to click;
-// [optional] 2: url should match given token; 3: should not click when certain cookie is already set; 4: disconnectTimeout.
+// [optional] 2: url should match given token; 3: disconnectTimeout.
 // example.com##+js(ceo, element)
-// example.com##+js(ceo, element, href, cookie, disconnectTimeout)
-// example.com##+js(ceo, element, , , disconnectTimeout)
+// example.com##+js(ceo, element, href, disconnectTimeout)
+// example.com##+js(ceo, element, , disconnectTimeout)
 (() => {
     let aelem = '{{1}}';
     if ( aelem === '' || aelem === '{{1}}' ) {
         return;
     }
-    // let bhref = '{{2}}';
-    // if ( bhref === ''  || bhref === '{{2}}' ) {
-    //     bhref = '';
-    // }
-    // let ccookie = '{{3}}';
-    // if ( ccookie === '' ) {
-    //     ccookie = -1;
-    // }
-    let dmsecs = '{{2}}';
-    if ( dmsecs === '{{2}}' ) {
+    let bhref = '{{2}}';
+    if ( bhref === ''  || bhref === '{{2}}' ) {
+        bhref = '';
+    }
+    let dmsecs = '{{3}}';
+    if ( dmsecs === '{{3}}' ) {
         dmsecs = '';
     }
     let etimeout = parseInt(dmsecs, 10);
     if ( isNaN(etimeout) || isFinite(etimeout) === false ) {
         etimeout = 10000;
     }
-    // if ( (window.location.href.indexOf(bhref) !== -1) && (document.cookie.indexOf(ccookie) == -1) ) {
+    if ( window.location.href.indexOf(bhref) !== -1 ) {
         const o = new MutationObserver(function() {
             const e = document.querySelector(aelem);
             e && (o.disconnect(), e.click())
@@ -160,5 +152,5 @@
         setTimeout(function() {
             o.disconnect()
         }, etimeout)
-    // }
+    }
 })();
